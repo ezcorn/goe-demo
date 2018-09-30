13a24e1e4f63d5949ae82d4b4a6a4555282134c1
ezcorn <ezcorn@outlook.com>
2018-09-30 11:09:47 +0800

    change reg codes by new version goe

--- server.go
+++ server.go
@@ -7,13 +7,9 @@ import (
 )
 
 func main() {
-	goe.RegAction(action.HelloWorldAction, func(a *goe.Action) {
-		a.Relate("check")
-	})
-	goe.RegAction(action.HelloWorld403Action, nil)
-	goe.RegListen(listen.JumpToHelloWorldListen, func(l *goe.Listen) {
-		l.Relate("/helloWorld403")
-	})
-
+	goe.RegAction(action.HelloWorldAction)
+	goe.RegAction(action.HelloWorld403Action)
+	goe.RegListen(listen.JumpToHelloWorldListen)
+	goe.RelateActionToListen("/helloWorld403", "jumpToHelloWorld")
 	goe.InitServer()
 }
